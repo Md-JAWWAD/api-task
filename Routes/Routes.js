b90b1f928fc7f7db05b4862c23fb218cba92227e
Muhammad Jawwad <themdjawwad@gmail.com>
2025-04-08 20:09:22 +0500

    Update Routes.js
    add function named as "getSearchProducts"

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,8 +1,15 @@
 import express from "express";
 const router = express.Router();
-import { createProducts, deleteProducts, getProducts, updateProducts } from "../Controllers/Controller.js"
+import {
+  createProducts,
+  deleteProducts,
+  getProducts,
+  updateProducts,
+  getSearchProducts,
+} from "../Controllers/Controller.js";
 
 router.get("/", getProducts);
+router.get("/:id", getSearchProducts);
 router.post("/", createProducts);
 router.put("/:id", updateProducts);
 router.delete("/:id", deleteProducts);
