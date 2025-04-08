531e383c676e8e6fcf8bd73cdb16dccbb05a6d67
Muhammad Jawwad <themdjawwad@gmail.com>
2025-04-08 20:08:00 +0500

    Update Controller.js
    Add query params on get requests

--- Controllers/Controller.js
+++ Controllers/Controller.js
@@ -3,8 +3,33 @@ import model from "../model/Schema.js";
 
 const getProducts = async (req, res) => {
   try {
-    const get = await model.find({});
-    res.status(200).json({ message: "Product found", get});
+    const filter = {};
+
+    if (req.query.name) {
+      filter.name = req.query.name;
+    }
+    if (req.query.class) {
+      filter.class = req.query.class;
+    }
+    if (req.query.profession) {
+      filter.profession = req.query.profession;
+    }
+
+    console.log(filter);
+    const getQuery = await model.find(filter);
+    res.json(getQuery);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+const getSearchProducts = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const collect = await model.find(id);
+    !collect
+      ? res.status(404).json({ message: "Product not find" })
+      : res.status(200).json({ message: "Product find", collect });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
@@ -15,7 +40,7 @@ const createProducts = async (req, res) => {
     const collect = await model.create(req.body);
     res.status(200).json({ message: "Product created", collect });
   } catch (error) {
-    res.status(200).json({ message: error.message });
+    res.status(404).json({ message: error.message });
   }
 };
 
@@ -42,4 +67,10 @@ const deleteProducts = async (req, res) => {
   }
 };
 
-export {getProducts, createProducts, updateProducts, deleteProducts}
+export {
+  getSearchProducts,
+  getProducts,
+  createProducts,
+  updateProducts,
+  deleteProducts,
+};
